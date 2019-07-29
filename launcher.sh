5f9ad03c63b3a67235935709b82e1b67e3d5def8
Derek Hilder <derek@oooh.tv>
2019-07-28 21:11:38 -0700

    Added comment to launcher script to help debug configuration issues

--- launcher.sh
+++ launcher.sh
@@ -4,5 +4,10 @@
 # All other environment variable substitutions work as expected
 sed -i"" "s/\${ES_HOST}/$ES_HOST/" /usr/share/logstash/pipeline/logstash.conf
 
+echo ""
+echo "Hacked ES_HOST into logstash.conf. Updated file:"
+cat /usr/share/logstash/pipeline/logstash.conf
+echo ""
+
 # Run logstash as normal with the hacked configuration
 exec logstash -f /usr/share/logstash/pipeline/logstash.conf
